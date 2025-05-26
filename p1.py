269d63c95b5dea4ed625c0536403b2bff76b0f88
Muzamil <muzamil@MacBook-Pro.local>
2025-05-26 16:43:55 -0400

    dot product

--- p1.py
+++ p1.py
@@ -1,17 +1,30 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
 weights1 = [0.2, 0.8, -0.5, 1.0]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [weights1, weights2, weights3]
+# we have 3 neurons in the layer, each with its own set of weights
 
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
+biases = [bias1, bias2, bias3]
 
+# This code calculates the output of a simple neural network layer
 # every input has its own unique weight but they all share the same bias
-output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-            inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-            inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
+
+# layer_outputs = []
+# for neuron_weights, bias in zip(weights, biases): # zip combines weights and biases
+#     neuron_output = 0
+#     for input_value, weight in zip(inputs, neuron_weights): # zip combines inputs and weights
+#         neuron_output += input_value * weight
+#     neuron_output += bias
+#     layer_outputs.append(neuron_output)
+
+output = np.dot(weights, inputs) + biases
 
 print(output)
 
